546eb92d961f1c4ad4e2ad90d2fda13886a584c7
codexvala-da <vala.da@northeastern.edu>
2024-09-09 19:21:13 -0400

    Revert "initial upload commit for lab0"
    This reverts commit a32ab506f47946c01a64df65b7a8f1c624489aa7.

--- src/Model/HelloWorld.java
+++ src/Model/HelloWorld.java
@@ -11,7 +11,7 @@ package Model;
 public class HelloWorld {
     
     public static void main (String[] args){
-        System.out.println("Hello,World");
+        System.out.println("Hello, World");
     }
     
 }
